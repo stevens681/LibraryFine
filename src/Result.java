95caa18c3ef8141fdb6f8a6978657821d35a44f2
fernandorosa <stevensfrr@gmail.com>
2022-07-19 20:22:12 -0400

    Initial commit

---
+++ src/Result.java
@@ -0,0 +1,26 @@
+public class Result {
+    /*
+     * Complete the 'libraryFine' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER d1
+     *  2. INTEGER m1
+     *  3. INTEGER y1
+     *  4. INTEGER d2
+     *  5. INTEGER m2
+     *  6. INTEGER y2
+     */
+
+    public static int libraryFine(int d1, int m1, int y1, int d2, int m2, int y2) {
+        // Write your code here
+        int res = 0;
+
+        return res;
+
+    }
+    public static void main(String[] args) {
+        System.out.println(libraryFine(9,6,2015,6,6,2015));
+
+    }
+}
